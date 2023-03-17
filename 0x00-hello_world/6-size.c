f208ab77897d371de409f5f8d0d3b12b809c7ddd
Salaheddine-Benabed <salah.benabed10@gmail.com>
2023-03-16 18:34:01 -0700

    4-puts.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)", sizeof(char));
 	printf("size of int: %lu byte(s)", sizeof(int));
 	printf("size of long int: %lu byte(s)", sizeof(long int));
-	prontf("size of a float: %lu byte(s)", sizeof(float));
+	printf("size of long long int: %lu byte(s)", sizeof(long long int));
+	printf("size of a float: %lu byte(s)", sizeof(float));
 	return (0);
 }
